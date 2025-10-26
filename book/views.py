e4f51b0eff5f5d7794c569a004aca14f7c5587d4
robiulhasananim <mdrobiulhasananim@gmail.com>
2025-10-26 20:48:56 +0600

    feat: add views

--- book/views.py
+++ book/views.py
@@ -1,3 +1,10 @@
+from book.forms import BookForm
+from book.models import Book
 from django.shortcuts import render
-
 # Create your views here.
+def book_create(request):
+    if request.method == 'POST':
+        pass
+    else:
+        form = BookForm()
+    return render(request, 'book/book_form.html', {'form': form})
