ae610d5b6ab1f73a71c4377cc00540c8e901bbbc
robiulhasananim <mdrobiulhasananim@gmail.com>
2025-10-27 23:42:27 +0600

    feat: add some functionality

--- book/forms.py
+++ book/forms.py
@@ -4,3 +4,20 @@ class BookForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = ['title', 'author', 'description','genre', 'isbn', 'publication_date']
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control','placeholder': 'Enter book title'}),
+            'author': forms.TextInput(attrs={'class': 'form-control','placeholder': 'Enter author name'}),
+            'description': forms. Textarea (attrs={'class': 'form-control','rows': 4,'placeholder': 'Enter book description'}),
+            'genre': forms.Select(attrs={
+            'class': 'form-control'
+            }),
+            'isbn': forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Enter ISBN'
+            }),
+            'publication_date': forms.DateInput(attrs={
+            'class': 'form-control',
+            'type': 'date'
+            })
+        }
