92a3e3b0802cd06690e5f3f66e5612ec2cc4be46
robiulhasananim <mdrobiulhasananim@gmail.com>
2025-10-28 00:23:08 +0600

    fix: add messages in form

--- book/views.py
+++ book/views.py
@@ -1,10 +1,15 @@
 from book.forms import BookForm
 from book.models import Book
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib import messages
 # Create your views here.
 def book_create(request):
     if request.method == 'POST':
-        pass
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success (request, 'Book created successfully!')
+            return redirect('book-create')
     else:
         form = BookForm()
     return render(request, 'book/book_form.html', {'form': form})
